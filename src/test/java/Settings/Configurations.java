33b1958a14ce42e76ddb90397ee1b4ea170f2fe9
Regonda Hosta <regonda555@mail.ru>
2021-08-30 16:15:24 +0300

    update browser Configurations

--- src/test/java/Settings/Configurations.java
+++ src/test/java/Settings/Configurations.java
@@ -1,47 +1,36 @@
 package Settings;
 
 import com.codeborne.selenide.Configuration;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
 import java.net.MalformedURLException;
-import java.net.URI;
+
+import java.util.HashMap;
 import java.util.Map;
 
-public class Configurations {
-    protected WebDriver driver;
-    /*
-    public void browserConfiguration(String browser, boolean headless) {
-        Configuration.browser = browser;
-        Configuration.headless = headless;
-        Configuration.startMaximized = true;
-        Configuration.timeout = 8000;
-    }
+public class Configurations{
 
-     */
-
-    public void browserConfiguration(String browser, boolean headless) throws MalformedURLException {
-        //Configuration.remote = "http://192.168.0.136:4444/wd/hub";
-        //Configuration.driverManagerEnabled = false;
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+    public void browserConfiguration(String browser) {
+        Configuration.remote = "http://192.168.0.136:4444/wd/hub";
         //capabilities.setCapability("browserName", browser);
-        capabilities.setCapability("browserName", "chrome");
-        capabilities.setCapability("browserVersion", "92.0");
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", false);
-        driver = new RemoteWebDriver(
-                URI.create("http://192.168.0.136:4444/wd/hub").toURL(),
-                capabilities
-        );
+
+        Map<String, Boolean> options = new HashMap<>();
+        options.put("enableVNC", true);
+        options.put("enableVideo", false);
+        options.put("enableLog", true);
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("92.0");
+        capabilities.setCapability("selenoid:options", options);
+        Configuration.browserCapabilities = capabilities;
 
     }
 
     @BeforeClass
     @Parameters("browser")
-    protected void setUp(@Optional("browser") String browser) throws MalformedURLException {
-        new Configurations().browserConfiguration(browser, true);
+    protected void setUp(@Optional("browser") String browser) {
+        new Configurations().browserConfiguration(browser);
     }
 
     @AfterMethod
