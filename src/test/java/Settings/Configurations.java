c469b2a066458889c1c6e2216dd2edb194c83860
Regonda Hosta <regonda555@mail.ru>
2021-08-31 14:35:06 +0300

    reset changes

--- src/test/java/Settings/Configurations.java
+++ src/test/java/Settings/Configurations.java
@@ -1,14 +1,10 @@
 package Settings;
 
 import com.codeborne.selenide.Configuration;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
 
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,13 +12,14 @@ public class Configurations{
 
     @BeforeClass
     @Parameters({"browser", "version"})
-    protected void setUp(@Optional("browser") String browser, @Optional("version") String version) throws MalformedURLException {
+    protected void setUp(@Optional("browser") String browser, @Optional("version") String version) {
         new Configurations().browserConfiguration(browser, version);
     }
 
 
-    public RemoteWebDriver browserConfiguration(String browser, String version) throws MalformedURLException {
+    public void browserConfiguration(String browser, String version) {
         Configuration.remote = "http://192.168.0.136:4444/wd/hub";
+
         Map<String, Boolean> options = new HashMap<>();
         options.put("enableVNC", true);
         options.put("enableVideo", false);
@@ -32,9 +29,7 @@ public class Configurations{
         capabilities.setCapability("browserName", browser);
         capabilities.setCapability("browserVersion", version);
         capabilities.setCapability("selenoid:options", options);
-        return new RemoteWebDriver(
-                URI.create("http://192.168.0.136:4444/wd/hub").toURL(),
-                capabilities);
+        Configuration.browserCapabilities = capabilities;
 
     }
 
