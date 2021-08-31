e4922ddd7b93a0c775a95b2f2a82ee12c5a4aaed
Regonda Hosta <regonda555@mail.ru>
2021-08-31 12:49:38 +0300

    Change browser Configuration

--- src/test/java/Settings/Configurations.java
+++ src/test/java/Settings/Configurations.java
@@ -3,35 +3,44 @@ package Settings;
 import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Configurations{
 
-    public void browserConfiguration(String browser, String version) {
-        Configuration.remote = "http://192.168.0.136:4444/wd/hub";
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+    @BeforeClass
+    @Parameters({"browser", "version"})
+    protected void setUp(@Optional("browser") String browser, @Optional("version") String version) {
+        new Configurations().browserConfiguration(browser, version);
+    }
 
+
+    public void browserConfiguration(String browser, String version) {
+        //Configuration.remote = "http://192.168.0.136:4444/wd/hub";
         Map<String, Boolean> options = new HashMap<>();
         options.put("enableVNC", true);
         options.put("enableVideo", false);
         options.put("enableLog", true);
 
-
+        DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("browserName", browser);
         capabilities.setCapability("browserVersion", version);
         capabilities.setCapability("selenoid:options", options);
-        Configuration.browserCapabilities = capabilities;
+        try {
+            RemoteWebDriver driver = new RemoteWebDriver(
+                    URI.create("http://selenoid:4444/wd/hub").toURL(),
+                    capabilities
+            );
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
-    }
-
-    @BeforeClass
-    @Parameters({"browser", "version"})
-    protected void setUp(@Optional("browser") String browser, @Optional("version") String version) {
-        new Configurations().browserConfiguration(browser, version);
     }
 
     @AfterMethod
