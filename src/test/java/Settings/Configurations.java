1b09301eb3592fcfe07f7e0ac5f866743b38f42b
Regonda Hosta <regonda555@mail.ru>
2021-08-31 17:30:11 +0300

    browser Configurations for selenium

--- src/test/java/Settings/Configurations.java
+++ src/test/java/Settings/Configurations.java
@@ -1,6 +1,7 @@
 package Settings;
 
 import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.*;
 
@@ -19,6 +20,8 @@ public class Configurations{
 
     public void browserConfiguration(String browser, String version) {
         Configuration.remote = "http://192.168.0.136:4444/wd/hub";
+        Configuration.browser = browser;
+        Configuration.browserVersion = version;
 
         Map<String, Boolean> options = new HashMap<>();
         options.put("enableVNC", true);
@@ -26,8 +29,8 @@ public class Configurations{
         options.put("enableLog", true);
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("browserName", browser);
-        capabilities.setCapability("browserVersion", version);
+        //capabilities.setCapability(CapabilityType.BROWSER_NAME, browser);
+        //capabilities.setCapability(CapabilityType.BROWSER_VERSION, version);
         capabilities.setCapability("selenoid:options", options);
         Configuration.browserCapabilities = capabilities;
 
